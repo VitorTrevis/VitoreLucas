4afd14cb1ac5b72a2e859359a8915b619c971feb
Vitor Trevisan <125533905+VitorTrevis@users.noreply.github.com>
2024-04-05 09:12:03 -0300

    commit Inicial

---
+++ projeto nutrilab/scripts/main.js
@@ -0,0 +1,5 @@
+/*!!!só pra deixar registrado o teste!!!const myHeading = document.querySelector("h1");
+myHeading.textContent = "Teste de Java Script";*/
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.botao_de_veja_mais', { duration: 4000});
+sr.reveal('.botao_de_fale_conosco_aqui', { duration: 4000});
